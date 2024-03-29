fcea629df217b9693542f385ea096225c92b883e
S566484 <S566484@nwmissouri.edu>
2024-03-28 20:44:29 -0500

    Completed layout of Journal List

--- app/src/main/java/com/example/happify/JournalList.java
+++ app/src/main/java/com/example/happify/JournalList.java
@@ -1,6 +1,8 @@
 package com.example.happify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class JournalList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+        JournalAdapter journalServer = new JournalAdapter();
+        RecyclerView journalRV = findViewById(R.id.journalRV);
+        journalRV.setAdapter(journalServer);
+        LinearLayoutManager myManager = new LinearLayoutManager(this);
+        journalRV.setLayoutManager(myManager);
     }
 }
