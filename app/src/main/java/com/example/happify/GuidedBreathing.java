44c8ccc2612fb465ce1422d3cb90cde2311deb6a
Saroz <31454493+spsaroj@users.noreply.github.com>
2024-04-07 14:27:56 -0500

    branch merge for management
    Merged Rikriti branch guided breathing

---
+++ app/src/main/java/com/example/happify/GuidedBreathing.java
@@ -0,0 +1,14 @@
+package com.example.happify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuidedBreathing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guided_breathing);
+    }
+}
