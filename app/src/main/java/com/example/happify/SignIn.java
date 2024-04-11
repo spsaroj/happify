2d4f734a0c672a253dfb34fc28080fe5022f9b1e
s556143 <143040522+s556143@users.noreply.github.com>
2024-04-11 17:31:08 -0500

    Signin page has been updated

--- app/src/main/java/com/example/happify/SignIn.java
+++ app/src/main/java/com/example/happify/SignIn.java
@@ -1,26 +1,42 @@
 package com.example.happify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignIn extends AppCompatActivity {
-
-    private ImageView submitLogo;
-    private TextView signInWithGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-    }
-    public void onSigninPressed(View v){
-        Intent signinIntent = new Intent(this, Dashboard.class);
-        startActivity(signinIntent);
+
+        
     }
 }
